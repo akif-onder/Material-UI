9febcd6eb84bcd4181cdce854217b178c25388e2
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-18 21:04:09 +0300

    MUI continued

--- material-intro/src/components/TypoButtons.jsx
+++ material-intro/src/components/TypoButtons.jsx
@@ -1,5 +1,50 @@
+import { Button, Container, Typography } from "@mui/material";
 const TypoButtons = () => {
-  return <div>TypoButtons</div>;
+  return (
+    <div>
+      <Container maxWidth="md">
+        <Typography
+          variant="h2"
+          component="h4"
+          sx={{
+            background: "lightgrey",
+            mt: 4,
+            border: "5px solid green",
+            align: "left",
+            color: "error",
+          }}
+        >
+          Typography, Buttons, Container, Box
+        </Typography>
+        <Typography variant="body1" align="justify">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          pariatur ea tempora ipsa ad illum, ipsum impedit, quibusdam unde
+          adipisci suscipit doloremque blanditiis non! Soluta, illum tenetur.
+          Reprehenderit, voluptatem sed!
+        </Typography>
+        <Typography
+          variant="button"
+          align="justify"
+          color="secondary"
+          sx={{ display: "inline-block", mt: 4 }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          pariatur ea tempora ipsa ad illum, ipsum impedit, quibusdam unde
+          adipisci suscipit doloremque blanditiis non! Soluta, illum tenetur.
+          Reprehenderit, voluptatem sed!
+        </Typography>
+      </Container>
+
+      <Container maxWidth="md">
+        <Button variant="contained" color="secondary">
+          CLICK
+        </Button>
+        <Button variant="outlined" color="success">
+          SEND
+        </Button>
+      </Container>
+    </div>
+  );
 };
 
 export default TypoButtons;
